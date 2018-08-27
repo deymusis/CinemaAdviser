bb73e858f83e8a5b2d4de30a55e2c146b1f29438
deymusis <vulpinari@yandex.ru>
2018-08-27 19:06:28 +0300

    Пытаюсь через ООП реализовать.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace CinemaAdviser
 {
     class Program
     {
+
         static void Main(string[] args)
         {
                 /*Программа поможет подобрать фильм, если ты уже не работаешь*/
@@ -17,91 +18,200 @@ namespace CinemaAdviser
 
                 String genre = "Comedy or Drama";
 
-            Console.WriteLine( "What is your name?" );
+                string [] names = {"Comedy", "Drama", "Erotic"};
+
+                
+            
+
+            Console.WriteLine( Resources.Name );
                 String name = Console.ReadLine(); // Поле ввода для твоего имени
             Console.WriteLine( "Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfWeek, date );
 
-            if (date.Hour >= endingHours)
-            {
-                Console.WriteLine("It's time to watch film!"); 
-                TimeToWatch(genre, name);
-            }
-            else
-            {
-                Console.WriteLine("You still must work!");
-                Console.WriteLine("Have a good day at work, {0}!", name);
-                Console.ReadLine();
-            }
+                if (date.Hour >= endingHours)
+                {
+                    Console.WriteLine("It's time to watch film!"); 
+                    TimeToWatch(genre, name);
+                }
+                else
+                {
+                    Console.WriteLine("You still must work!");
+                    Console.WriteLine("Have a good day at work, {0}!", name);
+                    Console.ReadKey();
+                }
         }
 
         /*Пора смотреть фильм!*/
 
-        private static void TimeToWatch(String genre, String name)
+        public static void TimeToWatch(String genre, String name)
         {
             Console.WriteLine("What movie would you like to watch? \n{0}", genre);
             String choiceOfGenre = Console.ReadLine();
             Console.Clear();
             Console.WriteLine("You could watch... Ehhh...");
 
-            if (choiceOfGenre == "Comedy")
-            {
-                Comedy();
-            }
-            else if (choiceOfGenre == "Drama")
-            {
-                Drama();
-            }
-            else if (choiceOfGenre == "Erotic")
-            {
-                Console.WriteLine("How old are you?");
-                String yearString = Console.ReadLine();
-                int years = Int32.Parse(yearString);
-                if (years < 18)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Unfortunately, it's only after the age of 18!");
-                    Console.WriteLine("Come back in " + (18 - years) + " years.");
+                switch (choiceOfGenre) {
+                    case "Comedy":
+                        Comedy();
+                        break;
+                    case "Drama":
+                        Drama();
+                        break;
+                    case "Erotic":
+                        Console.WriteLine("How old are you?");
+                        String yearString = Console.ReadLine();
+                        int years = Int32.Parse(yearString);
+                            if (years < 18)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Unfortunately, it's only after the age of 18!");
+                                Console.WriteLine("Come back in " + (18 - years) + " years.");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("These are going to be so passionate films!");
+                                Erotic();
+                            }
+                          break;
+                          default:
+                        Console.WriteLine("Wow! {0} - What is it? I don't know anything about it. sorry! i can't recommend anymore.", choiceOfGenre);
+                        break;
                 }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("These are going to be so passionate films!");
-                    Erotic();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Wow! {0} - what is it? I don't know anything about it. Sorry! I can't recommend anymore.", choiceOfGenre);
-            }
 
             Console.WriteLine("Enjoy your movie, {0}!", name);
-            Console.ReadLine();
-        }
-
-        /*Если выбрал Erotic*/
-
-        private static void Erotic()
-        {
-            Console.WriteLine("* Emmanuelle");
-            Console.WriteLine("* Fifty Shades of Grey");
-        }
-
-        /*Если выбрал Drama*/
-
-        private static void Drama()
-        {
-            Console.WriteLine("* Race");
-            Console.WriteLine("* Pelé: Birth of a Legend");
-            Console.WriteLine("* Snowden");
+            Console.ReadKey();
         }
 
         /*Если выбрал Comedy*/
 
+
+
         private static void Comedy()
         {
-            Console.WriteLine("* Furry Vengeance");
-            Console.WriteLine("* Oz the Great and Powerful");
-            Console.WriteLine("* Beethoven");
+            string[] films = {"Furry Vengeance",
+                              "Oz the Great and Powerful",
+                              "Beethoven",
+                              "Best in Show,",
+                              "In the Loop",
+                              "Bridesmaids",
+                              "The Layover",
+                              "L’embarras du choix",
+                              "Magic in the Moonlight",
+                              "Life as We Know It",
+                              "Bridget Jones’s Baby",
+                              "Populaire",
+                              "Pitch Perfect",
+                              "Spy",
+                              "Les émotifs anonymes",
+                              "L’arnacoeur",
+                              "Love & Friendship",
+                              "Leap Year"
+                             };
+
+            foreach (string i in films)
+            {
+                Console.WriteLine("* {0}", i);
+            }
+            RandomFilm(films);
         }
+
+        /*Если выбрал Drama*/
+
+            
+            
+        private static void Drama()
+        {
+            string[] films = {"Race",
+                              "Pelé: Birth of a Legend",
+                              "Snowden",
+                              "Babel",
+                              "Dancer in the Dark",
+                              "Rain man",
+                              "М",
+                              "Schindler’s List",
+                              "Captain Fantastic",
+                              "We Need to Talk About Kevin",
+                              "The Believer",
+                              "La vita è bella",
+                              "A Time to Kill"
+                             };
+
+            foreach (string i in films)
+            {
+                Console.WriteLine("* {0}", i);
+            }
+            RandomFilm(films);
+        }
+
+        /*Если выбрал Erotic*/
+
+        public static void Erotic()
+        {
+            
+            string[] films = {"Emmanuelle",
+                              "Fifty shades of grey",
+                              "Newness",
+                              "Kiki",
+                              "Swinger",
+                              "Nymphomaniac",
+                              "No strings attached",
+                              "My awkward sexual adventure",
+                              "3d rou pu tuan zhi ji le bao jian",
+                              "Nuit",
+                              "The rules of attraction",
+                              "Caligola",
+                              "Trasgredire",
+                              "La vie d'adèle",
+                              "Les valseuses",
+                              "Turks fruit",
+                              "Salò o le 120 giornate di sodoma",
+                              "Lucía y el sexo"
+                             };
+
+            foreach (string i in films)
+            {
+                Console.WriteLine("* {0}", i);
+            }
+            RandomFilm(films);
+        }
+
+        
+
+        /*Выбирает случайный фильм*/
+
+        private static void RandomFilm(string[] names)
+        {
+            Console.WriteLine("I recommend - {0}", names[new Random().Next(0, names.Length)]);
+        }
+  
     }
+
+        class Genre {
+            public string[] Names { get; set; }
+            public string[] Films { get; set; }
+
+            public Genre(string[] names, string[] films) {
+                Names = names;
+                Films = films;
+            }
+
+
+
+            /*Никак не могу применить этот метод для каждого жанра*/
+
+            public void ShowFilms()
+            {
+                foreach (string i in Films)
+                {
+                    Console.WriteLine("* {0}", i);
+                }
+                RandomFilm();
+            }
+
+            private void RandomFilm()
+            {
+                Console.WriteLine("I recommend - {0}", Films[new Random().Next(0, Films.Length)]);
+            }
+        }
+
 }
